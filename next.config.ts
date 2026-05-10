28e5976a88484035a35fae63fc1a71c87d02b697
gmlduqzhd <gmlduqzhd123@gmail.com>
2026-05-10 23:04:03 +0900

    feat: apply real data from litt.ly

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'image.yes24.com' },
+      { protocol: 'https', hostname: 'img.youtube.com' },
+      { protocol: 'https', hostname: 'file.bookk.co.kr' },
     ],
   },
 };
